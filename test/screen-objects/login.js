e39a863f01b661e7e23738e0a56b8657660ce754
Wim Selles <wim@wswebcreation.nl>
2019-03-19 15:56:09 +0100

    init commit

--- test/screen-objects/login.js
+++ test/screen-objects/login.js
@@ -1,5 +1,40 @@
 class LoginScreen {
 
+    get LoginScreen(){
+        return $('~test-Login');
+    }
+
+    get Username(){
+        return $('~test-Username');
+    }
+
+    get Password(){
+        return $('~test-Password');
+    }
+
+    get LoginButton(){
+        return $('~test-LOGIN');
+    }
+
+    get ErrorMessageContainer(){
+        return $('~test-Error message');
+    }
+
+    waitForShown(){
+        return this.LoginScreen.waitForDisplayed(15000);
+    }
+
+    signIn(data){
+        const {username, password} = data;
+
+        this.Username.addValue(username);
+        this.Password.addValue(password);
+        this.LoginButton.click();
+    }
+
+    waitForErrorMessageShown(){
+        return this.ErrorMessageContainer.waitForDisplayed(15000);
+    }
 }
 
 export default new LoginScreen();
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,8 +1,23 @@
+import { getTextOfElement } from '../helpers/utils'
+import LoginScreen  from '../screen-objects/login'
+import { LOGIN_USERS } from '../helpers/e2eConstants'
+
 describe('Login', () => {
+    beforeEach(()=> browser.reset());
+
     it('should be able to login with a standard user', () => {
+        expect(LoginScreen.waitForShown()).toEqual(true);
+        LoginScreen.signIn(LOGIN_USERS.STANDARD)
+        expect($('~test-PRODUCTS').waitForDisplayed(15000)).toEqual(true);
+        console.log('text = ', $$('~test-Item')[0].getText())
     });
 
-    xit('should not be able to login with a locked user', () => {
+    it('should not be able to login with a locked user', () => {
+        expect(LoginScreen.waitForShown()).toEqual(true);
+        expect(LoginScreen.ErrorMessageContainer.isDisplayed()).toEqual(false)
+        LoginScreen.signIn(LOGIN_USERS.LOCKED);
+        expect(LoginScreen.waitForErrorMessageShown()).toEqual(true);
+        expect(getTextOfElement(LoginScreen.ErrorMessageContainer)).toContain('Sorry, this user has been locked out')
     });
 
     xit('should show an error when no username is provided', () => {
